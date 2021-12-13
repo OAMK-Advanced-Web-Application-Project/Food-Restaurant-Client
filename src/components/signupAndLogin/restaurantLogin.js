7f9010c3fd714ef3abb7a8ca799edce7af5d231d
sarafarahabadi <sarafarahabadi@gmail.com>
2021-12-13 22:16:39 +0200

    27th

--- src/components/signupAndLogin/restaurantLogin.js
+++ src/components/signupAndLogin/restaurantLogin.js
@@ -22,6 +22,11 @@ export default function RestaurantLogin(props) {
           username: usernameLog,
           password: passwordLog,
         },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }
     );
     localStorage.setItem("token", result.data.token);
